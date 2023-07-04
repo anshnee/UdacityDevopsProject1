c37241c75a71308f77e309e7b526e2f809041cb2
Anshu <anshu.ranjan2607@mail.com>
2023-07-04 18:03:52 -0500

    Added nsg rules for VM to recieve HTTP data on port 8080

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,6 +20,17 @@ resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
 }
 
